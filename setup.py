dde3ded9e9e20edfb84d1f5408541331ee926f55
MariaUsuga <105682206+MariaUsuga@users.noreply.github.com>
2025-03-02 07:28:20 +0000

    carga inicial

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bigdata_actividad_1",
+    version="0.0.1",
+    author="Maria Arenas",
+    author_email="",
+    description="",
+    py_modules=["actividad_1"],
+    install_requires=[
+        "pandas",
+        "openpyxl",
+        "requests"
+    ]
+    
+    
+)
